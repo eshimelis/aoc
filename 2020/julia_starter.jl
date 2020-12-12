c126f1bea384638475b3dd36d2cdfdf30059200b
Eyassu Shimelis <eshimelis@hmc.edu>
2020-12-12 10:10:16 -0500

    basic starter julia template

---
+++ 2020/julia_starter.jl
@@ -0,0 +1,29 @@
+# Day X: 
+
+using AOC
+
+
+# Part 1 -
+
+function solve(input::Array)
+    return nothing
+end
+
+inputs = readlines(joinpath(@__DIR__, "test.txt"))
+solution = solve(inputs)
+println("Part 2 Solution")
+println("   : ", solution)
+
+# ===================================================================
+
+# Part 2 -
+
+function solve(input::Array)
+    return nothing
+end
+
+inputs = readlines(joinpath(@__DIR__, "test.txt"))
+solution = solve(inputs)
+println("Part 2 Solution")
+println("   : ", solution)
+
