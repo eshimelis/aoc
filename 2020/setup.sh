1570220a4615799accdf31ced7a4d1f7aedeec60
Eyassu Shimelis <eshimelis@hmc.edu>
2020-12-12 10:10:24 -0500

    directory setup script

---
+++ 2020/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+while getopts d: flag
+do 
+    case "${flag}" in
+        d) day=${OPTARG};;
+    esac
+done
+echo "Setting up Day $day directory...";
+
+# make folder and populate with starter files
+folder=day_$day/;
+mkdir $folder;
+
+# create input and test files
+julia_script=${folder}/day_${day}.jl;
+input_file=${folder}/input.txt;
+test_file=${folder}/test.txt;
+
+touch $input_file
+touch $test_file
+# touch $julia_script
+
+# initialize julia script with starter file
+starter_file=./julia_starter.jl
+cp ./julia_starter.jl  ${julia_script}
+
+echo "Done"
