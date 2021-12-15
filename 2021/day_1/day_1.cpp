9f683f3fafa44d123494d4adaf427204a2a305a2
eshimelis <eshimelis@hmc.edu>
2021-12-15 18:29:12 -0500

    add common

--- 2021/day_1/day_1.cpp
+++ 2021/day_1/day_1.cpp
@@ -1,35 +1,38 @@
-# Day X: 
+// Day 1: Sonar Sweep
+// Author: Eyassu Shimelis
+// Date: 05 DEC 2021
 
-using AOC
-using Logging
+#include <iostream>
+#include <vector>
 
-LOG_LEVEL = Logging.Info
-logger = ConsoleLogger(stdout, LOG_LEVEL)
-global_logger(logger)
+#include "spdlog/spdlog.h"
+#include "../common/AOC.h"
 
-# Part 1 -
+int solve_1(AOCInput input) {
+  int solution = 0;
 
-function solve(input::Array)
-    return nothing
-end
+  return solution;
+}
 
-inputs = readlines(joinpath(@__DIR__, "test.txt"))
-output = solve(inputs)
-solution = output
-println("Part 1 Solution")
-println("   : ", solution, "\n")
+int solve_2(AOCInput input) {
+  int solution = 0;
 
-# ===================================================================
+  return solution;
+}
 
-# Part 2 -
+// int day_1(std::string input_filepath)
 
-function solve(input::Array)
-    return nothing
-end
+int main(int argc, char **argv) {
+  std::cout << "hello, world!" << std::endl;
 
-inputs = readlines(joinpath(@__DIR__, "test.txt"))
-output = solve(inputs)
-solution = output
-println("Part 2 Solution")
-println("   : ", solution)
+  std::cout << "you have entered " << argc << " inputs" << std::endl;
 
+  for (int i = 0; i < argc; i++)
+  {
+    std::cout << argv[i] << std::endl;
+  }
+  auto part_1 = AOCProblem(1);
+  auto part_2 = AOCProblem(2);
+
+  return 0;
+}
